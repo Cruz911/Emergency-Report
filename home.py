0eb8ab45abd33cbbca445002567dc926a88c6094
Cruz911 <39281580+Cruz911@users.noreply.github.com>
2019-03-22 10:10:58 +0200

    Add files via upload

---
+++ home.py
@@ -0,0 +1,50 @@
+import kivy
+kivy.require('1.9.1')
+
+from kivy.app import App
+from kivy.uix.button import Label
+from kivy.uix.stacklayout import StackLayout
+from kivy.uix.popup import Popup
+from pymongo import MongoClient
+import os
+
+class homeAction(StackLayout):
+
+    def loginFunction(self, username, password):
+        if username and password:
+            try:
+                client = MongoClient()
+                db = client['remindersNote']
+                cursor = db.users.find({"username" : username}).count()
+                if cursor > 0:
+                    dataUser = db.users.find({"username" : username, "password" : password}).count()
+                    if dataUser > 0:
+                        #popup = Popup(title='Message', content=Label(text="Account Authenticate"), size_hint=(None, None), size=(200, 80))
+                        os.system("python report.py")
+                    else:
+                        popup = Popup(title='Message', content=Label(text="Incorrect Password"), size_hint=(None, None), size=(200, 80))
+                else:
+                    popup = Popup(title='Message', content=Label(text="Incorrect Input"), size_hint=(None, None), size=(200, 80))
+                popup.open()
+            except Exception:
+                self.display.text = "Error Input"
+        else:
+            self.display.text = "Required Field"
+
+    def registerApp(self):
+        os.system("python register.py")
+
+    def forgotApp(self):
+        os.system("python forgotpass.py")
+
+    def reportApp(self):
+        os.system("python main.py")
+
+class homeLog(App):
+
+    def build(self):
+        return homeAction()
+
+homelog = homeLog()
+
+homelog.run()
